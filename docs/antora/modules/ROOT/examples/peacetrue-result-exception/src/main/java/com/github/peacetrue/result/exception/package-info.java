27c3a806cf76969c63102ac97e9026a7d67d2eda
peacetrue <xiayouxue@gmail.com>
2022-08-15 14:31:09 +0800

    修改文档说明

--- docs/antora/modules/ROOT/examples/peacetrue-result-exception/src/main/java/com/github/peacetrue/result/exception/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 所有方法的参数和返回值默认都不能为空，如果可以为空，使用 {@link javax.annotation.Nullable} 标注
- *
- * @author peace
- **/
-@NonNullApi
-package com.github.peacetrue.result.exception;
-
-import org.springframework.lang.NonNullApi;
